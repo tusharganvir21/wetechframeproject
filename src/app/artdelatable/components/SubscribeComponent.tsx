238b65e0f7110ad36d853a5330f90b685d3ebb0d
unknown <tushar.ganvir21@vit.edu>
2024-12-10 00:26:45 +0530

    fixed issue

--- src/app/artdelatable/components/SubscribeComponent.tsx
+++ src/app/artdelatable/components/SubscribeComponent.tsx
@@ -18,12 +18,12 @@ const SubscribeComponent: React.FC = () => {
       <div className="flex flex-col justify-center bg-[#FFF3F9] p-8 rounded-[20px] w-full max-w-[818px] lg:h-[300px] lg:ml-5">
         {/* Heading */}
         <h2 className="text-[35px] lg:text-[43px] font-medium font-cabinet-grotesk text-[#393939]">
-          S’inscrire & économiser <span className="text-[#5CD2DD]">10%</span>
+          S&apos;inscrire & économiser <span className="text-[#5CD2DD]">10%</span>
         </h2>
 
         {/* Subheading */}
         <p className="text-[14px] font-geist mt-4 text-[#BDA2B0] mb-6">
-          Office ipsum you must be muted. Synergize helicopter prioritize anyway job due harvest most opportunity didn't.
+          Office ipsum you must be muted. Synergize helicopter prioritize anyway job due harvest most opportunity didn&apos;t.
           Yet busy any meeting shark light marginalised 4-blocker message. Productize corporate nail caught synergy
           highlights lunch. Company another pushback items dear or any.
         </p>
@@ -36,7 +36,7 @@ const SubscribeComponent: React.FC = () => {
             className="p-3 rounded-md w-full max-w-[650px] font-geist text-[#A68A98] border border-[#F5E1EB] focus:outline-none focus:ring-2 focus:ring-[#5CD2DD]"
           />
           <button className="flex items-center font-cabinet-grotesk bg-[#5CD2DD] text-white px-6 py-3 rounded-md mt-4 lg:mt-0">
-            <span className="mr-2">S’inscrire</span>
+            <span className="mr-2">S&apos;inscrire</span>
             <ArrowForward />
           </button>
         </div>
